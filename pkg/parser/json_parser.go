6c8b310f8938aec62fcd7d90a5b1649465a3a926
mdryaan <alikhurshid842001@gmail.com>
2025-12-22 14:20:00 +0530

    fix(parser): handle missing optional fields gracefully

--- pkg/parser/json_parser.go
+++ pkg/parser/json_parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/mdryaan/netmapper/internal/models"
 )
@@ -11,7 +12,12 @@ type JSONParser struct{}
 func (p *JSONParser) Parse(data []byte) (*models.Topology, error) {
 	var topology models.Topology
 	if err := json.Unmarshal(data, &topology); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse JSON: %w", err)
+	}
+	for i := range topology.Nodes {
+		if topology.Nodes[i].Status == "" {
+			topology.Nodes[i].Status = models.NodeStatusActive
+		}
 	}
 	return &topology, nil
 }
--- pkg/parser/yaml_parser.go
+++ pkg/parser/yaml_parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/mdryaan/netmapper/internal/models"
 	"gopkg.in/yaml.v3"
 )
@@ -10,7 +12,12 @@ type YAMLParser struct{}
 func (p *YAMLParser) Parse(data []byte) (*models.Topology, error) {
 	var topology models.Topology
 	if err := yaml.Unmarshal(data, &topology); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse YAML: %w", err)
+	}
+	for i := range topology.Nodes {
+		if topology.Nodes[i].Status == "" {
+			topology.Nodes[i].Status = models.NodeStatusActive
+		}
 	}
 	return &topology, nil
 }
